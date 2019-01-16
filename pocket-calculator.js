99080b81f156411b64743dd4a61a39901ea38611
Damian Roginski <definitelynotdamian@gmail.com>
2019-01-16 13:24:49 -0500

    css and js

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,1 +1,8 @@
-function
+//Make Pi 9 digits long//
+function input(num) {
+  document.calc.display.value = document.calc.display.value + num;
+}
+function equal(){
+  let equationEqual = document.calc.display.value
+  document.calc.display.value = eval(equationEqual)
+}
